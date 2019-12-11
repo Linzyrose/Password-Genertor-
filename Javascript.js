ba622a6283bb4c408ef31aae3ad94b6ddc5e6f17
Linzyrose <56662532+Linzyrose@users.noreply.github.com>
2019-12-10 18:36:36 -0700

    updated

---
+++ Javascript.js
@@ -0,0 +1,51 @@
+let passwordLength = prompt("How long do you want your password?");
+console.log(passwordLength)
+
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let userChoices = []
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const symbol  = ["!", "@"]
+const numbers = [1,2,3,4,5,6,7,8,9,0]
+let userPassword = ""
+
+let upperCaseAnswer = confirm ("Do you want upper case letters?")
+console.log (upperCaseAnswer)
+let lowerCaseAnswer = confirm ("Do you want lower case letters?")
+let symbolAnswer = confirm ("Do you want smybols?")
+let numberAnswer = confirm ("Do you want numbers?")
+
+if (upperCaseAnswer) {
+    console.log ("its true")
+userChoices.push (upperCase)
+console.log (userChoices)
+}
+
+if (lowerCaseAnswer) {
+    console.log ("its true")
+userChoices.push (lowerCase)
+console.log (userChoices)
+}
+
+if (symbolAnswer) {
+    console.log ("its true")
+userChoices.push (symbol)
+console.log (userChoices)
+}
+
+if (numberAnswer) {
+    console.log ("its true")
+userChoices.push (numbers)
+console.log (userChoices)
+}
+
+let newChoices = Array.prototype.concat.apply([],userChoices)
+console.log (newChoices)
+
+for (i = 0; i <passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() *newChoices.length)
+    console.log (randomNumber)
+    userPassword += newChoices[randomNumber]
+}
+
+var passwordArea = document.getElementById ("password")
+passwordArea.value= userPassword
